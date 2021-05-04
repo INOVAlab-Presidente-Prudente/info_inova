1d850b4515f174e65b9234d050d9dc6db82026a9
rxngui <mateus2015men@outlook.com>
2021-05-04 09:54:27 -0300

    Corrigido alguns erros na listagem de ocorrências

--- pages/ocorrencias.php
+++ pages/ocorrencias.php
@@ -1,6 +1,6 @@
 <?php include("../includes/header.php")?>
 <body class="hold-transition sidebar-mini" onload="document.title='Admin Page | Ocorrências'">
-    <?php include("../includes/navbar.php") ?>
+    <?php include("../includes/navbar.php"); $output = "";?>
     <?php include("../includes/sidebar.php") ?>
     <div class="wrapper">
         <div class="content-wrapper">
@@ -20,62 +20,58 @@
                         <div class="col-md-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h2 class="card-title"><strong> </strong></h2>
+                                    <div class="card-title w-100">
+                                        
+                                        <?php
+                                            if(isset($_GET['excluido']))
+                                                $output = "Ocorrência excluída com sucesso"; //ALERT EXCLUIDO
+                                            if(isset($_GET['erroExcluir']))
+                                                $output = "Erro ao excluir ocorrência"; //ALERT
+                                            if($output != "")
+                                                echo "<div class='alert alert-success ' role='alert'>".$output."</div>";
+                                        ?>
+                                        
+                                    </div>
                                 </div>
                                 <div class="card-body">
                                     <button type='button' class='btn btn-info w-100 mb-4' onclick="window.location.href = 'cadastrarOcorrencia.php?usu_id=<?=$_GET['usu_id']?>'">Nova ocorrência</button>
-                                    
-                                    <div class="container">
-                                        <?php
-                                            if(isset($_GET['excluido']))
-                                                echo "excluido"; //ALERT EXCLUIDO
-                                            if(isset($_GET['erroExcluir']))
-                                                echo "erro ao excluir"; //ALERT
-                                            require_once("../admin/DB.php");
-                                            $sql = "SELECT usu_nome FROM usuario WHERE usu_id = '".$_GET['usu_id']."'";
-                                            $query = mysqli_query($connect, $sql);
-                                            if($query){
-                                                $row = mysqli_fetch_assoc($query);
-                                                echo "<h4 class='mb-4'>Todas Ocorrências de <strong>".$row['usu_nome']."</strong></h4>";
-                                                
-                                                $sql = "SELECT * FROM ocorrencia WHERE usu_id = '".$_GET['usu_id']."'";
-                                                $query = mysqli_query($connect, $sql);
-                                                $res = mysqli_fetch_array($query);
-                                                if($res == null)
-                                                    echo " O usuário não possui ocorrência!";//".$row['usu_nome']."
-                                                $cont = 0;
-                                                echo "<div class='row'>";
-                                                while($res != null){
-                                                    if($cont < 4){
-                                                        echo "<div class='col-md-3 ml-1 mb-2'>";
-                                                            echo "<div class='card card-outline card-info' style='width:14rem;'>";
-                                                            echo "<div class='card-body'>";
-                                                                echo "<div class='card-title'>Data:".$res['oc_data']."</div><br><div class='card-subtitle mb-2 text-muted'><strong>Ocorrência: </strong>".$res['oc_descricao']."</div>";
-                                                                    echo "<div class='card-tools'";
-                                                                        echo "<a style='cursor: pointer;' type='link' name='alterar' onclick=\"window.location.href='alterarOcorrencia.php?oc_id=".$res['oc_id']."'\"><i class='fas fa-edit mr-4'></i></a>";
-                                                                        echo "<a style='cursor: pointer;' type='link' name='excluir' onclick=\"excluir(".$res['oc_id'].",".$_GET['usu_id'].")\"><i class='fas fa-trash'></i></a>";
-                                                                    echo "</div>";
-                                                                echo "</div>";
-                                                            echo "</div>";
-                                                        echo "</div>";
-                                                        if($cont == 3)
-                                                            echo "</div>";
-                                                        $cont++;
-                                                    }
-                                                    else{
-                                                        $cont = 0;
-                                                        echo "<div class='row'>";
-                                                    }
-                                                    
-                                                    
-                                                    
+                                    <?php    
+                                        require_once("../admin/DB.php");
+                                        $sql = "SELECT usu_nome FROM usuario WHERE usu_id = '".$_GET['usu_id']."'";
+                                        $query = mysqli_query($connect, $sql);
+                                        if($query){
+                                            $row = mysqli_fetch_assoc($query);?>
+                                            <h4>Todas Ocorrências de <strong><?=$row['usu_nome']?></strong></h4>
+                                        <div class="container">
+                                            <div class="row">
+                                                <?php 
+                                                    $sql = "SELECT * FROM ocorrencia WHERE usu_id = '".$_GET['usu_id']."'";
+                                                    $query = mysqli_query($connect, $sql);
                                                     $res = mysqli_fetch_array($query);
                                                     if($res == null)
+                                                        echo " <h3>O usuário não possui ocorrência! </h3>";//".$row['usu_nome']."
+                                                    
+                                                    while($res != null){
+                                                        $data = substr($res['oc_data'],0,-9);
+                                                        $horario = substr($res['oc_data'],-9,11);
+                                                        echo "<div class='col mb-3'>";
+                                                            echo "<div class='card card-outline card-info' style='width:14rem;'>";
+                                                            echo "<div class='card-body'>";
+                                                                echo "<div class='row'> <h5 class='card-title'> <strong>Data:</strong> ".$data."</h5></div>  <div class='row'><h6 class=' card-subtitle mb-3 text-muted mt-1'><strong>Horario:</strong>".$horario." </h6></div>  <div class='row card-text text-truncate'><div class='font-weight-bold'>Ocorrência: </div> ".$res['oc_descricao']."</div>";
+                                                                    echo "<div class='mt-1 row justify-content-between'>";
+                                                                        echo "<a class='col-4 btn btn-light w-25' type='link' name='alterar' onclick=\"window.location.href='alterarOcorrencia.php?oc_id=".$res['oc_id']."'\"><i class='fas fa-edit'></i></a>";
+                                                                        echo "<a class='col-4 btn btn-light w-25' type='link' name='excluir' onclick=\"excluir(".$res['oc_id'].",".$_GET['usu_id'].")\"><i class='fas fa-trash'></i></a>";
+                                                                        echo "</div>";
+                                                                    echo "</div>";
+                                                            echo "</div>";
                                                         echo "</div>";
-                                                }
-                                            }else echo "Usuario não encontrado";
-                                        ?>
-                                    </div>
+                                                        $res = mysqli_fetch_array($query);
+                                                        
+                                                    }
+                                                }else $output = "Usuario não encontrado";
+                                                ?>
+                                            </div>
+                                        </div>
                                 </div>
                                 <!-- <div class="card-footer">
                                 
