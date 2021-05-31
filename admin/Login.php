345dfdb230175dae01e81d0a607e2fcb0a4bcc40
rf-ferreira <r4fael.s0uza@gmail.com>
2021-05-31 18:02:46 -0300

    Adicionado reCAPTCHA
    Co-authored-by: Mateus Mendonça Monteiro <rxngui@users.noreply.github.com>
    Co-authored-by: Santanicius <Santanicius@users.noreply.github.com>

--- admin/Login.php
+++ admin/Login.php
@@ -14,60 +14,76 @@ $query = mysqli_query($connect, $sql);
 
 $fetch = mysqli_fetch_assoc($query);
 if ($fetch != null) {
-    if (password_verify($senha, $fetch['usu_senha'])) {
-        $_SESSION['logado'] = true;
-        $_SESSION['cpf'] =  $fetch['usu_cpf'];
-        $_SESSION['nome'] =  $fetch['usu_nomedeusuario'];
-        
-        switch ($fetch['pu_id']) {
-            case "1":
-                $_SESSION['admin'] = true;
-                $_SESSION['perfil_usuario'] = "Administrador";
-            break;
-            case "2":
-                $_SESSION['coworking'] = true;
-                $_SESSION['perfil_usuario'] = "Coworking";
-            break;
-            case "3":
-                $_SESSION['usuario'] = true;
-                $_SESSION['perfil_usuario'] = "Visitante";
-            break;
-            case "4":
-                $_SESSION['financeiro'] = true;
-                $_SESSION['perfil_usuario'] = "Financeiro";
-            break;
-            case "5":
-                $_SESSION['evento'] = true;
-                $_SESSION['perfil_usuario'] = "Evento";
-            break;
-            case "6":
-                $_SESSION['empresa'] = true;
-                $_SESSION['perfil_usuario'] = "Empresa";
-            break;
+    if (password_verify($senha, $fetch['usu_senha']) && isset($_POST['g-recaptcha-response'])) {
+        require_once("SecretKey.php");
+        $secretkey = SECRET_KEY;
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response = $_POST['g-recaptcha-response'];
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretkey."&response=".$response;
+        $success = json_decode(file_get_contents($url))->success;
+
+        if ($success) {
+            $_SESSION['logado'] = true;
+            $_SESSION['cpf'] =  $fetch['usu_cpf'];
+            $_SESSION['nome'] =  $fetch['usu_nomedeusuario'];
+            
+            switch ($fetch['pu_id']) {
+                case "1":
+                    $_SESSION['admin'] = true;
+                    $_SESSION['perfil_usuario'] = "Administrador";
+                break;
+                case "2":
+                    $_SESSION['coworking'] = true;
+                    $_SESSION['perfil_usuario'] = "Coworking";
+                break;
+                case "3":
+                    $_SESSION['usuario'] = true;
+                    $_SESSION['perfil_usuario'] = "Visitante";
+                break;
+                case "4":
+                    $_SESSION['financeiro'] = true;
+                    $_SESSION['perfil_usuario'] = "Financeiro";
+                break;
+                case "5":
+                    $_SESSION['evento'] = true;
+                    $_SESSION['perfil_usuario'] = "Evento";
+                break;
+                case "6":
+                    $_SESSION['empresa'] = true;
+                    $_SESSION['perfil_usuario'] = "Empresa";
+                break;
+            }
+            if (isset($_SESSION['logado']) && !isset($_SESSION['admin']) && !isset($_SESSION['coworking']) && !isset($_SESSION['financeiro'])){
+                session_destroy();
+                echo "<div class='alert alert-danger alert-dismissible'>
+                    <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                    <h5><i class='fas fa-skull-crossbones'></i>&nbspAcesso Negado!</h5>
+                        <p class='text-break'>Você não tem acesso ao sistema!.</p>
+                </div>";
+            }else if ($fetch['usu_primeiro_login']){
+                $_SESSION['primeiro_login'] = true;
+                header("location: ../pages/alterarSenha.php");
+            }else{
+                header("location: ../pages/adminPage.php");}
+        }
+        else{
+            echo "<div class='alert alert-danger alert-dismissible'>
+                    <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+                    <i class='fas fa-robot'></i></i> Complete o CAPTCHA para entrar!
+                </div>";
         }
 
-        if (isset($_SESSION['logado']) && !isset($_SESSION['admin']) && !isset($_SESSION['coworking']) && !isset($_SESSION['financeiro'])){
-            session_destroy();
-            echo "<div class='alert alert-danger alert-dismissible'>
-                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-                <h5><i class='fas fa-skull-crossbones'></i>&nbspAcesso Negado!</h5>
-                    <p>Você não tem acesso ao sistema!.</p>
-              </div>";
-        }else if ($fetch['usu_primeiro_login']){
-            $_SESSION['primeiro_login'] = true;
-            header("location: ../pages/alterarSenha.php");
-        }else{
-            header("location: ../pages/adminPage.php");}
+        
     } else  
         echo "<div class='alert alert-danger alert-dismissible'>
                 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
                 <h5><i class='icon fas fa-ban'></i> Dados Incorretos!</h5>
-                    <p>Senha ou email incorretos, verifique o que você digitou e tente novamente.</p>
+                    <p class='text-break'>Email ou senha incorretos, verifique e tente novamente.</p>
               </div>";
 } else{
     echo "<div class='alert alert-warning alert-dismissible'>
             <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
             <h5><i class='icon fas fa-exclamation-triangle'></i> Dados Inexistentes!</h5>
-                <p>Dados não encontrados no sistema, entre em contato com o administrador do sistema para se cadastrar.</p>
+                <p class='text-break'>Dados não encontrados no sistema, entre em contato com o administrador do sistema para se cadastrar.</p>
           </div>";
 }
--- index.php
+++ index.php
@@ -1,130 +1,92 @@
 <?php 
     ob_start();
     session_start();
+    
     if (isset($_SESSION['logado'])) {
       header("location: pages/adminPage.php");
     }
+    /* header('Access-Control-Allow-Origin: \*'); */
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>InfoInova - Login</title>
-  <link rel="sortcut icon" href="images/logo_page.png" type="image/x-icon" />
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  
-</head>
-<body class="hold-transition login-page">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>InfoInova - Login</title>
+    <link rel="sortcut icon" href="images/logo_page.png" type="image/x-icon" />
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
 
-<script type="text/javascript">
-    
-    /*var verifyCallback = function(response) {
-        var URL_TO_FETCH = 'https://www.google.com/recaptcha/api/siteverify?secret=&response='+ response;
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+  </head>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="#"><b>Info</b>Inova</a>
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
       
-        $.post(URL_TO_FETCH).done(function(result){
-            console.log(result);
-        });
-        
-        const resultFectch = fetch(URL_TO_FETCH, {method:'POST', mode:'no-cors', 
-            header: {'Accept': 'application/json', 'Content-Type': 'application/json'},
-            body: JSON.stringify({secret:'', response:response})
-        }).then(function(resposta){
-            console.log(resposta);
-        }).catch(function(error){
-            console.log("Response reCAPTCHA error: " + error.toString());
-        });
-        
-        console.log(resultFectch);
-    };
-    
-    var errorCallback = function(response){
-        
-    };*/
-    
-    /*var onloadCallback = function() {
+        <div class="card-body login-card-body">
+          <div class="mb-2">
+            <?php 
+              if (isset($_POST['entrar'])) {
+                  require_once("admin/Login.php");
+              }
+            ?>
+          </div>
+          <p class="login-box-msg">Faça login para iniciar sua sessão</p>
+
+          <form method="post">
+            <div class="input-group mb-3">
+              <input type="email" name = "email" class="form-control" placeholder="example@email.com">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" name ="senha" placeholder="Senha">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+
+            <div class="input-group">
+                <div class="col w-100 mx-auto">
+                  <div id="g-recaptcha" data-sitekey="6LfOv_kaAAAAAEafHopd2IGULE3LDMUH1byxx5ya"></div>
+                </div>
+            </div>
+                <br>
+                <button type="submit" name="entrar" class="btn btn-warning btn-block fo">Login</button>
+          </form>
+          
+      </div>
+    </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+    <script>
+    var onloadCallback = function() {
         grecaptcha.render('g-recaptcha', {
-          'sitekey' : '',
-          'theme' : 'light',
-          /*'callback' : verifyCallback,
-          'error-callback' : errorCallback*/
-        });
-    };*/
-  </script>
-  <div class="login-box">
-    <div class="login-logo">
-      <a href="#"><b>Info</b>Inova</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-    
-      <div class="card-body login-card-body">
-        <div class="mb-2">
-          <?php 
-            if (isset($_POST['entrar'])) {
-                require_once("admin/Login.php");
-            }
-          ?>
-        </div>
-        
-        <p class="login-box-msg">Faça login para iniciar sua sessão</p>
-
-        <form method="post">
-          <div class="input-group mb-3">
-            <input type="email" name = "email" class="form-control" placeholder="example@email.com">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" name ="senha" placeholder="Senha">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col w-100">
-            <div class="col w-100 mx-auto">
-                <div id="g-recaptcha"></div>
-              </div>
-            <br>
-              <button type="submit" name="entrar" class="btn btn-warning btn-block fo">Login</button>
-            </div>
-            <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-            async defer></script>
-            
-            
-          </div>
-        </form>
-        
-    </div>
-  </div>
-
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
-  <script src="https://www.google.com/recaptcha/api.js?render=reCAPTCHA_site_key"></script>
-  <script>
-    function onSubmit(token) {
-      document.getElementById("demo-form").submit();
-    }
- </script>
-  
-</body>
+          'sitekey' : '6LfOv_kaAAAAAEafHopd2IGULE3LDMUH1byxx5ya',
+          'theme' : 'light'
+        })}
+    </script>
+  </body>
 </html>
